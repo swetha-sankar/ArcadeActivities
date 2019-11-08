449da8fb71d93b955ba2d541141e9506ad3cb334
Swetha Sankar <swetha@udel.edu>
2019-11-07 20:01:10 -0500

    ada potato game, bank status, ada status from class

--- bank_account.py
+++ bank_account.py
@@ -1,6 +1,29 @@
-
-
 # Bank accounts have a name and a balance
+class BankAccount:
+    name: str
+    balance: int
 
+    def __init__(self, account_name: str, initial_balance: int):
+        self.name = account_name
+        self.balance = initial_balance
+
+    def deposit(self, amount: int):
+        self.balance += amount
+
+    def withdraw(self, amount:int):
+        if amount > self.balance:
+            old_balance = self.balance
+            self.balance = 0
+            return old_balance
+        else:
+            self.balance -= amount
+            return amount
+
+
+swetha_points = BankAccount("UD points", 100)
+lauren_points = BankAccount("UD points", 20)
+
+swetha_points.deposit(200)
+print("You have", swetha_points.balance, "dollars")
 
 # SafeAccount - cannot be overdrawn
--- doggos.py
+++ doggos.py
@@ -9,3 +9,43 @@ Dogs have breeds
 Cats have coat color
     hunt
 '''
+
+
+class Dog:
+    # class makes a new type
+    name: str
+    energy: int
+    hungry: bool
+    mood: str
+
+    def __init__(self, the_name: str):
+        # method: function that lives in a class. special purpose: constructor.
+        # constructs instances (in this case: dogs). name is an attribute of all dogs.
+        # type of dog always the same as the type of class (this is b/c of the "self")
+        self.name = the_name
+        self.energy = 0
+        self.hungry = False
+        self.mood = "happy"
+
+    def feed(self):
+        self.hungry = False
+
+    def play_with_dog(self):
+        self.mood = "happy"
+        self.hungry = True
+        self.energy -= 1
+
+    def rest(self, length_of_time: int):
+        self.energy += length_of_time
+
+
+ada = Dog("Ada")
+ada.rest(8)
+ada.play_with_dog()
+ada.rest(5)
+print(ada.mood)
+
+babbage = Dog("Babbage")
+babbage.mood = ada.mood
+
+
